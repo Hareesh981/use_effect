a9c23116932c62d3411829ebee0d1f288b6a85bd
manikanta1011 <memanikanta10@gmail.com>
2024-12-07 12:24:49 +0530

    added map and css

--- task/src/project/New.jsx
+++ task/src/project/New.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./index.css";
  
-function FetchData() {
+function New() {
   const [data, setData] = useState([]);
  const [error, setError] = useState("")
   
  useEffect(() => {
   fetch('./data.json')
     .then((response) => response.json())
-    .then((data) => setUsers(data))
-    .catch((error) => console.error("Error fetching data:", error));
+    .then((data) => setData(data))
+    .catch((error) => setError("Error fetching data:", error));
 }, []); 
 if(error){
   return <h2>{error}</h2>
@@ -18,9 +18,18 @@ if(error){
 <div className="container">
 <h1>Player List</h1>
 
+<ul className="card-list">
+        {data.map((player) => (
+<li key={player.id} className="card">
+<h2>{player.name}</h2>
+<p>{player.age}</p>
+</li>
+        ))}
+</ul>
+
 </div>
   );
 }
  
-export default FetchData;
+export default New;
 
