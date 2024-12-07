42a683200723fa92e7a22e6dc7a9f9492ca93e8c
Hareesh 981 <hareeshgiddanti@gmail.com>
2024-12-07 12:06:50 +0530

    created the component named fetch data in useEffect code

---
+++ task/src/project/New.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import "./index.css";
+ 
+function FetchData() {
+  const [data, setData] = useState([]);
+ const [error, setError] = useState("")
+  
+ 
+  return (
+<div className="container">
+<h1>Player List</h1>
+
+</div>
+  );
+}
+ 
+export default FetchData;
+
